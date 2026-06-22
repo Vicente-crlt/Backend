9db4f5d442106fc504364f600a25492fcdc5cddb
Vicente-crlt <vicente.pereira6@aluno.ce.gov.br>
2026-06-22 20:06:07 -0300

    Add port configuration for database connection

--- database/connection.js
+++ database/connection.js
@@ -1,9 +1,10 @@
 const mysql = require("mysql2/promise");
-require("dotenv").config();
 
 const db = mysql.createPool({
 
     host: process.env.DB_HOST,
+
+    port: process.env.DB_PORT,
 
     user: process.env.DB_USER,
 
