c6f17d7c078308558092cc8ea1d7f2f5a67877d6
Vicente-crlt <vicente.pereira6@aluno.ce.gov.br>
2026-06-22 21:07:09 -0300

    Change database connection to use fixed credentials
    Updated database connection settings to use hardcoded values.

--- database/connection.js
+++ database/connection.js
@@ -2,15 +2,11 @@ const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
 
-    host: process.env.DB_HOST,
-
-    port: process.env.DB_PORT,
-
-    user: process.env.DB_USER,
-
-    password: process.env.DB_PASSWORD,
-
-    database: process.env.DB_NAME,
+     host: "reseau.proxy.rlwy.net",
+    port: 32650,
+    user: "root",
+    password: "vHTikQqHAcotfWeNpNZAkkAukaKlFopb",
+    database: "railway"
 
     waitForConnections: true,
 
